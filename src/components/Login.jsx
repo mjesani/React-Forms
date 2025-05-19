a2cf7dc3c398aebffda4a744941ab0da1909c10e
mjesani <60170133+mjesani@users.noreply.github.com>
2025-05-19 19:43:02 +0530

    Getting form input using  individual state and 2 way binding function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Handle login logic here
+    console.log("Logging in with", { email, password });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={handleEmailChange} value={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={handlePasswordChange} value={password}/>
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button"
+          //type="button" // can be used tp prevent server form submission. Another way is to use onSubmit on form tag with event.preventDefault()
+        >Login</button>
       </p>
     </form>
   );
