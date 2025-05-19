0714db45d96092cd44af89801f767866344a20e2
mjesani <60170133+mjesani@users.noreply.github.com>
2025-05-19 20:01:31 +0530

    Get form values using native browser api FormData().
    - A straigtforward approach would be to use browser api called FormData and extracting values of all inputs out of it using fromEntries method.
    - This requires that name attribute is assigned to all html inputs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from './components/Header.jsx';
-import LoginRef from './components/LoginRef.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <LoginRef />
+        <Signup />
       </main>
     </>
   );
